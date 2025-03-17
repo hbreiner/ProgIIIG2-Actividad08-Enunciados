442e3dcf62839897ceeb1afce9f7bbe1158eaa74
Breiner <hbreyner4@gmail.com>
2025-03-17 19:55:08 +0000

    Agregue las ultimas dos consultas de hermana y tia

--- ProgIIIG2-Actividad08-Enunciados.pl
+++ ProgIIIG2-Actividad08-Enunciados.pl
@@ -19,3 +19,9 @@ progenitor(patricia, jaime).
 
 % a) es_madre(X) :- X es madre si X es mujer y progenitora de alguien.
 es_madre(X) :- mujer(X), progenitor(X, _).
+
+% d) hermana_de(X, Y) :- X es hermana de Y si X es mujer, tienen el mismo padre o madre y son diferentes personas.
+hermana_de(X, Y) :- mujer(X), progenitor(Z, X), progenitor(Z, Y), dif(X, Y).
+
+% g) tia(X, Y) :- X es tía de Y si X es hermana de alguno de los progenitores de Y.
+tia(X, Y) :- hermana_de(X, Z), progenitor(Z, Y).
